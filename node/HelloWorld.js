dd233b194724d6866aa10877e4804f49e1f930f4
_Enoabasi_Essien_ <enoabasidream@gmail.com>
2023-01-19 09:29:11 +0400

    Delete HelloWorld.js

--- node/HelloWorld.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-import { createServer } from "http";
-
-createServer(function (req, res) {
-	res.writeHead(200, { "Content-Type": "text/html" });
-	res.end(
-		"Hello, World -- My name is Enoabasi Essien\n I am running late for classification class."
-	);
-}).listen(8080);
